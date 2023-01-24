ad0fb1c9b1414fda0e60e6a05e11773cea85747e
ankit <ankitbhardwaj11411920@gmail.com>
2023-01-25 04:58:42 +0530

    my notes 25-1-23

--- wd/47 express/49 mongodb/49.3 deleting items from the mongo db.js
+++
@@ -1,9 +0,0 @@
-// deleting items from the mongo database
-
-db.items.find({field4:"value4"})
-
-db.items.deleteOne({field4:"value4"})
-// #162.1 deleteOne will delete the matching document entry and will delete the first entry in case of multi document match.
-
-db.items.deleteMany({field4:"value4"})
-// #162.2 deleteMany will delete all the matching document entry.
